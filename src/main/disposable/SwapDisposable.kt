e2f9910a16591544d2ea9721772592f40319b9c1
Praveen Mathew <praveen.mathew92@outlook.com>
2020-10-17 15:42:11 +0530

    Get disposable from the swap disposable

---
+++ src/main/disposable/SwapDisposable.kt
@@ -0,0 +1,28 @@
+package disposable
+
+import reactor.core.Disposable
+
+class SwapDisposable: Disposable.Swap {
+    companion object {
+        val DEFAULT_DISPOSABLE = BasicDisposable()
+    }
+    private var disposable: Disposable? = null
+
+    override fun update(next: Disposable?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun replace(next: Disposable?): Boolean {
+        disposable = next
+        return true
+    }
+
+    override fun get(): Disposable {
+        return disposable?: DEFAULT_DISPOSABLE
+    }
+
+    override fun dispose() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ src/test/disposable/SwapDisposableTest.kt
@@ -0,0 +1,28 @@
+import disposable.BasicDisposable
+import disposable.SwapDisposable
+import disposable.SwapDisposable.Companion.DEFAULT_DISPOSABLE
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class SwapDisposableTest : DisposableTest<SwapDisposable> {
+    override fun createDisposable(): SwapDisposable {
+        return SwapDisposable()
+    }
+
+    @Test
+    fun `should return default diposable if no disosable is present`() {
+        val swapDisposable = SwapDisposable()
+        assertEquals(DEFAULT_DISPOSABLE, swapDisposable.get())
+    }
+
+    @Test
+    internal fun `should get disposable added by replace`() {
+        val disposable = BasicDisposable()
+        val swapDisposable = SwapDisposable()
+        val response = swapDisposable.replace(disposable)
+
+        assertEquals(disposable, swapDisposable.get())
+        assertTrue(response)
+    }
+}
